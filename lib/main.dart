35590c2af6811bc69e83638b19fb997208b9a9a4
ibrahimq21 <ibrahimq21@gmail.com>
2019-10-04 03:18:11 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'dart:async';
+import 'dart:convert';
+import 'surah_list.dart';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+
+
+
+void main(){
+  runApp(IslamApp());
+}
+
+
+
+class IslamApp extends StatelessWidget{
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Display Lis of Surah',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.black,
+          displayColor: Colors.grey[600],
+        ),
+        // This colors the [InputOutlineBorder] when it is selected
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
+      ),
+      home: SurahList(),
+    );
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Future<Post> fetchPost() async {
+  final response =
+  await http.get('https://jsonplaceholder.typicode.com/posts/2');
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON.
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+
+
+class MyApp extends StatelessWidget {
+  final Future<Post> post;
+
+  MyApp({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: post,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.body);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/surah_list.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SurahList extends StatefulWidget{
+
+
+
+
+
+  @override
+  _SurahListState createState() => _SurahListState();
+
+
+}
+
+class _SurahListState extends State<SurahList> {
+
+
+  final _surahList = <String>[];
+
+  static const _surahNames = <String>[
+    'Al-Faatiha',
+    'Al-Baqara',
+    'Aal-i-Imraan',
+    'An-Nisaa',
+    'Al-Maaida',
+    'Al-An\' aam',
+    'Al-A\'raaf',
+    'Al-Anfaal',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
